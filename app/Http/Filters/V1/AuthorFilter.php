7aa693a6a48e12a8ff8e19c72ecea058680a6b10
Nuno Correia <nuno_correia_96@hotmail.com>
2025-05-02 17:49:12 +0200

    feat: update AuthorFilter class in laravel-apis project
    - Add $sortable property for name, email, createdAt, updatedAt
    - Update method structure for better readability
    - Correct 'email' method to search 'email' instead of 'title'
    - Improve code formatting for consistency

--- app/Http/Filters/V1/AuthorFilter.php
+++ app/Http/Filters/V1/AuthorFilter.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Filters\V1;
 
-class AuthorFilter extends QueryFilter
-{
-    public function createdAt($value)
-    {
+class AuthorFilter extends QueryFilter {
+    protected $sortable = [
+        'name',
+        'email',
+        'createdAt' => 'created_at',
+        'updatedAt' => 'updated_at'
+    ];
+
+    public function createdAt($value) {
         $dates = explode(',', $value);
 
         if (count($dates) > 1) {
@@ -15,30 +20,25 @@ class AuthorFilter extends QueryFilter
         return $this->builder->whereDate('created_at', $value);
     }
 
-    public function include($value)
-    {
+    public function include($value) {
         return $this->builder->with($value);
     }
 
-    public function id($value)
-    {
+    public function id($value) {
         return $this->builder->whereIn('id', explode(',', $value));
     }
 
-    public function email($value)
-    {
+    public function email($value) {
         $likeStr = str_replace('*', '%', $value);
-        return $this->builder->where('title', 'like', $likeStr);
+        return $this->builder->where('email', 'like', $likeStr);
     }
 
-    public function name($value)
-    {
+    public function name($value) {
         $likeStr = str_replace('*', '%', $value);
         return $this->builder->where('name', 'like', $likeStr);
     }
 
-    public function updatedAt($value)
-    {
+    public function updatedAt($value) {
         $dates = explode(',', $value);
 
         if (count($dates) > 1) {
@@ -47,4 +47,6 @@ class AuthorFilter extends QueryFilter
 
         return $this->builder->whereDate('updated_at', $value);
     }
+
+
 }
